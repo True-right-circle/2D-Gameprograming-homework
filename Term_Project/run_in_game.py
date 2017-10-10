993d5fb34d03d502b5efe279fc5fff981b09d4ef
woowon123 <wlsdndnjs12@naver.com>
2017-10-11 00:53:40 +0900

    Spacebar를 눌러 캐릭터 점프 기능 구현

--- Term_Project/run_in_game.py
+++ Term_Project/run_in_game.py
@@ -4,9 +4,10 @@ from pico2d import*
 
 name='MainState'
 image=None
+jump_time=0.0
+jump_check=0.0
 
 class Background:
-    
     def __init__(self):
         self.image=load_image('back_pink.png')
  
@@ -15,20 +16,29 @@ class Background:
 
 class Main_Char:
     def __init__(self):
-        self.x,self.y=50,80
-        self.frame=0
-        self.image=load_image('new_char_sheet.png')
+        self.x,self.y=50,90
+        self.run_frame=0
+        self.jump_frame=0
+        self.run_image=load_image('new_char_sheet.png')
+        self.jump_image=load_image('jump_sheet.png')
 
     def update(self):
-        self.frame=(self.frame+1)%8
+        self.run_frame=(self.run_frame+1)%8
+        self.jump_frame=(self.jump_frame+1)%2
         
     def draw(self):
-        self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
+        global jump
+        if(jump==True):
+            self.run_image.clip_draw(self.run_frame*100,0,100,100,self.x,self.y)
+        elif(jump==False):
+            self.jump_image.clip_draw(self.jump_frame*100,0,100,100,self.x,self.y)
+            
     
 def enter():
-    global image,background,running,boy
+    global image,background,running,boy,jump
     boy=Main_Char()
     background = Background()
+    jump=True
     running=True
     image=None
 
@@ -39,21 +49,34 @@ def exit():
     del(background)
     
 def handle_events():
-    global running
+    global running,jump,jump_time
     events=get_events()
     for event in events:
         if event.type==SDL_QUIT:
             game_framework.quit()
+        elif(event.type,event.key)==(SDL_KEYDOWN,SDLK_SPACE):
+            jump=False
         else:
             if(event.type,event.key)==(SDL_KEYDOWN,SDLK_ESCAPE):
                 running=False
                 if(event.type,event.key)==(SDL_KEYDOWN,SDLK_ESCAPE):
                     game_framework.quit()
-
-            
+                    
+        
 def update():
+    global jump,jump_time,jump_check
     boy.update()
-    
+    if(jump==False and jump_time<=2.0):
+        jump_time+=1.0
+        boy.y+=75.0
+        jump_check+=75.0
+    if(jump_time>=3.0):
+        jump=True
+        jump_time=0.0
+    if(jump_check>=0):
+        boy.y-=25.0
+        jump_check-=25.0
+         
 def draw():
     while running:
         update()
