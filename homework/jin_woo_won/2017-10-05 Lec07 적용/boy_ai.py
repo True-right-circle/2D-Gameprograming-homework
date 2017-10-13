d717646c723736d2c0d210ae1b5ca7d66b5c0c51
woowon123 <wlsdndnjs12@naver.com>
2017-10-13 22:09:50 +0900

    main_state와 boy_ai로 나눔

---
+++ homework/jin_woo_won/2017-10-05 Lec07 적용/boy_ai.py
@@ -0,0 +1,79 @@
+import game_framework
+import main_state
+import random
+from pico2d import*
+
+name='AI'
+member =5
+image = None
+font = None
+char_font = None
+team = None
+n = 0
+
+class Grass:
+    def __init__(self):
+        self.image=load_image('grass.png')
+ 
+    def draw(self):
+        self.image.draw(400,30)
+        
+class Boy:
+    image = None
+    global n,team
+    LEFT_RUN,RIGHT_RUN,LEFT_STAND,RIGHT_STAND =0,1,2,3
+
+    def handle_left_run(self):
+        self.x-=5
+        self.run_frames+=1
+        if self.x<0:
+            self.state=self.RIGHT_RUN
+            self.x=0
+        if self.run_frames == 100:
+            self.state=self.LEFT_STAND
+            self.stand_frames=0
+            
+    def handle_left_stand(self):
+        self.stand_frames+=1
+        if self.stand_frames==50:
+            self.state=self.LEFT_RUN
+            self.run_frames=0
+            
+    def handle_right_run(self):
+        self.x+=5
+        self.run_frames+=1
+        if self.x>800:
+            self.state=self.LEFT_RUN
+            self.x=800
+        if self.run_frames == 100:
+            self.state=self.RIGHT_STAND
+            self.stand_frames=0
+            
+    def handle_right_stand(self):
+        self.stand_frames+=1
+        if self.stand_frames==50:
+            self.state=self.RIGHT_RUN
+            self.run_frames=0
+
+    handle_state={
+        LEFT_RUN: handle_left_run,
+        RIGHT_RUN: handle_right_run,
+        LEFT_STAND:handle_left_stand,
+        RIGHT_STAND: handle_right_stand
+        }        
+    def __init__(self):
+        self.x,self.y=random.randint(100,700),random.randint(90,500)
+        self.frame= random.randint(0,7)
+        self.dir=1
+        self.run_frames=0
+        self.stand_frames=0
+        self.state = random.randint(0,3)
+        if Boy.image==None:
+            Boy.image=load_image('animation_sheet.png')
+        
+    def update(self):
+        self.frame=(self.frame+1)%8
+        self.handle_state[self.state](self)
+        
+    def draw(self):
+        self.image.clip_draw(self.frame*100,self.state*100,100,100,self.x,self.y)
--- homework/jin_woo_won/2017-10-05 Lec07 적용/main_state.py
+++ homework/jin_woo_won/2017-10-05 Lec07 적용/main_state.py
@@ -1,82 +1,16 @@
+from boy_ai import*
 import game_framework
 import random
 from pico2d import*
 import title_state
 
 
+
 name = 'MainState'
-image = None
+
 font = None
-team = None
-n = 0
-class Grass:
-    def __init__(self):
-        self.image=load_image('grass.png')
- 
-    def draw(self):
-        self.image.draw(400,30)
-        
- 
-class Boy:
-    image = None
-    global n,team
-    LEFT_RUN,RIGHT_RUN,LEFT_STAND,RIGHT_STAND =0,1,2,3
-
-    def handle_left_run(self):
-        self.x-=5
-        self.run_frames+=1
-        if self.x<0:
-            self.state=self.RIGHT_RUN
-            self.x=0
-        if self.run_frames == 100:
-            self.state=self.LEFT_STAND
-            self.stand_frames=0
-            
-    def handle_left_stand(self):
-        self.stand_frames+=1
-        if self.stand_frames==50:
-            self.state=self.LEFT_RUN
-            self.run_frames=0
-            
-    def handle_right_run(self):
-        self.x+=5
-        self.run_frames+=1
-        if self.x>800:
-            self.state=self.LEFT_RUN
-            self.x=800
-        if self.run_frames == 100:
-            self.state=self.RIGHT_STAND
-            self.stand_frames=0
-            
-    def handle_right_stand(self):
-        self.stand_frames+=1
-        if self.stand_frames==50:
-            self.state=self.RIGHT_RUN
-            self.run_frames=0
-
-    handle_state={
-        LEFT_RUN: handle_left_run,
-        RIGHT_RUN: handle_right_run,
-        LEFT_STAND:handle_left_stand,
-        RIGHT_STAND: handle_right_stand
-        }        
-    def __init__(self):
-        self.x,self.y=random.randint(100,700),random.randint(90,500)
-        self.frame= random.randint(0,7)
-        self.dir=1
-        self.run_frames=0
-        self.stand_frames=0
-        self.state = random.randint(0,3)
-        if Boy.image==None:
-            Boy.image=load_image('animation_sheet.png')
-        
-    def update(self):
-        self.frame=(self.frame+1)%8
-        self.handle_state[self.state](self)
-        
-    def draw(self):
-        self.image.clip_draw(self.frame*100,self.state*100,100,100,self.x,self.y)
-        font.draw(580,560,'Boy Num ='+str(n))
+char_font = None
+  
 
 
 def handle_mouseevents():
@@ -90,10 +24,11 @@ def handle_mouseevents():
 
 
 def enter():
-    global team,grass,boy,n,running,image,font
-    team=[Boy() for i in range(1000)] 
+    global team,grass,boy,n,running,image,font,char_font
+    team=[Boy() for i in range(member)] 
     grass = Grass()
-    font=load_font('ENCR10B.TTF',30)
+    font=load_font('ENCR10B.TTF',50)
+    char_font=load_font('ENCR10B.TTF',25)
     running=True
     image=None
 
@@ -104,6 +39,7 @@ def exit():
     del(grass)
 
 def handle_events():
+    global team
     events=get_events()
     for event in events:
         if event.type==SDL_QUIT:
@@ -111,7 +47,7 @@ def handle_events():
 
 def update():
     global team
-    for i in range(1000):
+    for i in range(member):
         team[i].update()
         
 def draw():
@@ -121,13 +57,14 @@ def draw():
         update()
         clear_canvas()
         grass.draw()
-        for i in range(1000):
-            team[i].draw()
+        font.draw(730,560,str(n))
+        char_font.draw(team[n].x-50,team[n].y+50,'This Boy')
+        char_font.draw(team[n].x-50,team[n].y-15,str(n))
+        for i in range(member):
+            team[i].draw()   
         update_canvas()
         delay(0.05)
         handle_mouseevents()
-
-        
         
 def pause():
     pass
@@ -142,13 +79,13 @@ def select():
         if event.type == SDL_KEYDOWN and event.key == SDLK_UP:
             n+=1
             print(n)
-            if n>=1000:
+            if n>member-1:
                 n=0
         elif event.type == SDL_KEYDOWN and event.key == SDLK_DOWN:
             n-=1
             print(n)
-            if n<=0:
-                n=999
+            if n<=-1:
+                n=member-1
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running=False
             game_framework.change_state(title_state)
