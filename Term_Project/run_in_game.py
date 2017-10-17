ef6eb0e25a52d0af9634b8b49786a98f3025e534
woowon123 <wlsdndnjs12@naver.com>
2017-10-17 23:07:19 +0900

    적,장애물 sprite 7 animation 오브젝트 추가 & 장애물 적 이벤트 추가

--- Term_Project/run_in_game.py
+++ Term_Project/run_in_game.py
@@ -8,6 +8,51 @@ image=None
 jump_time=0.0
 jump_check=0.0
 speed=5.0
+enemyspeed =18.0
+blockspeed=8.0
+
+class enemie:
+     def __init__(self):
+        self.x,self.y=1400,90
+        self.run_frame=0
+        self.run_image=load_image('flying_sheet.png')
+        
+     def update(self):
+         global flying
+         self.run_frame=(self.run_frame+1)%8
+         flying.x-=enemyspeed
+         if(flying.x<0):
+             flying.x=1400
+         
+     def draw(self):
+         self.run_image.clip_draw(self.run_frame*100,0,100,100,self.x,self.y)
+    
+class breadblock:
+     def __init__(self):
+        self.x,self.y=1200,200
+        self.image=load_image('block1.png')
+        
+     def update(self):
+         block1.x-=enemyspeed
+         if(block1.x<-100):
+             block1.x=1400
+             
+     def draw(self):
+        self.image.draw(self.x,self.y)
+        
+class grayblock:
+     def __init__(self):
+        self.x,self.y=1200,60
+        self.image=load_image('block2.png')
+        
+     def update(self):
+         block2.x-=blockspeed
+         if(block2.x<-100):
+             block2.x=1400
+             
+     def draw(self):
+        self.image.draw(self.x,self.y)         
+        
 class nBackground:
     def __init__(self):
         self.image=load_image('back_pink.png')
@@ -50,14 +95,14 @@ class Main_Char:
         self.jump_frame=(self.jump_frame+1)%2
         if(jump==False and jump_time<=2.0):
             jump_time+=1.0
-            boy.y+=75.0
-            jump_check+=75.0
+            boy.y+=95.0
+            jump_check+=95.0
         if(jump_time>=3.0):
             jump=True
             jump_time=0.0
         if(jump_check>=0):
-            boy.y-=25.0
-            jump_check-=25.0
+            boy.y-=35.0
+            jump_check-=35.0
         
     def draw(self):
         global jump
@@ -68,20 +113,26 @@ class Main_Char:
             
     
 def enter():
-    global image,background,running,boy,jump,nextbackground
+    global image,background,running,boy,jump,nextbackground,flying,block1,block2
     boy=Main_Char()
     background = Background()
     nextbackground=nBackground()
+    flying=enemie()
+    block1=breadblock()
+    block2=grayblock()
     jump=True
     running=True
     image=None
 
     
 def exit():
-    global background,boy,nextbackground
+    global background,boy,nextbackground,flying,block1,block2
     del(boy)
     del(background)
     del(nextbackground)
+    del(flying)
+    del(block1)
+    del(block2)
     
 def handle_events():
     global running,jump,jump_time
@@ -99,10 +150,13 @@ def handle_events():
                     
         
 def update():
-    global jump,jump_time,jump_check,background
+    global jump,jump_time,jump_check,background,flying,block1,block2
     boy.update()
     background.update()
     nextbackground.update()
+    flying.update()
+    block1.update()
+    block2.update()
          
 def draw():
     while running:
@@ -111,6 +165,9 @@ def draw():
         background.draw()
         nextbackground.draw()
         boy.draw()
+        flying.draw()
+        block1.draw()
+        block2.draw()
         update_canvas()
         handle_events()
         delay(0.05)
