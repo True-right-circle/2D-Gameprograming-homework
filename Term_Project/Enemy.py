5c7a92b40df1e608b067a93725e1fd77b0cb8c2d
woowon123 <wlsdndnjs12@naver.com>
2017-11-22 04:40:18 +0900

    충돌체크 적용
    get_bb() 충돌체크 flying에 적용

--- Term_Project/Enemy.py
+++ Term_Project/Enemy.py
@@ -3,22 +3,23 @@ from run_in_game import*
 from pico2d import*
 from math import*
 
-radius=700
-angle=0
+
 
 class enemy:
      def __init__(self):
-        self.x,self.y=1000,(random.randint(9,13)*10)
+        self.x,self.y=850,60#(random.randint(5,10)*10)
         self.run_frame=0
         self.run_image=load_image('flying_sheet.png')
         
-     def update(self,enemyspeed,check_time):
-         global radius,angle
-         angle+=0.02
+     def update(self,enemyspeed):
+         self.x-=enemyspeed
          self.run_frame=(self.run_frame+1)%8
-         self.x=400+(radius*cos(angle))
-         self.y=-480+radius*sin(angle)
- 
+         if(self.x<-50):
+              self.x=850
+
      def draw(self):
          self.run_image.clip_draw(self.run_frame*100,0,100,100,self.x,self.y)
+         
+     def get_bb(self):
+          return self.x-5,self.y-15,self.x+5,self.y+10      
     
--- Term_Project/run_in_game.py
+++ Term_Project/run_in_game.py
@@ -32,35 +32,31 @@ class Main_Char:
         global jump,jump_time,jump_check,background ,running,check_time
         self.run_frame=(self.run_frame+1)%6
         self.jump_frame=(self.jump_frame+1)%6
-        if flying.y<=90 and flying.x == boy.x and boy.y<=90:
-            running=False
-            font.draw(300,400,"Game Over")
-            font.draw(270,350,"Score : ")
-            font.draw(410,350,str(check_time))
-            font.draw(470,350,"Second ")
 
         if(jump==False and jump_time<3.0):
             jump_time+=0.6
-            boy.y+=20.0
+            boy.y+=40.0
             jump_check+=40.0
         if(jump_time>=3.0):
             jump=True
         if(jump_check>=0):
-            boy.y-=10.0
+            boy.y-=20.0
             jump_check-=20.0
         if(jump_time==4.0):
              boy.y+=0.0
              jump_check+=0.0
         if(jump_check==0):
              jump_time=0.0
-  
+    
     def draw(self):
         global jump
         if(jump==True):
             self.run_image.clip_draw(self.run_frame*100,0,100,55,self.x,self.y)
         elif(jump==False):
             self.jump_image.clip_draw(self.jump_frame*100,0,100,55,self.x,self.y)
-            
+
+    def get_bb(self):
+        return self.x-5,self.y-5,self.x+5,self.y+15        
             
     
 def enter():
@@ -102,16 +98,34 @@ def handle_events():
                 if(event.type,event.key)==(SDL_KEYDOWN,SDLK_ESCAPE):
                     #running=False
                     game_framework.quit()
-                    
+
+def collide(a,b):
+    left_a,bottom_a,right_a,top_a=a.get_bb()
+    left_b,bottom_b, right_b,top_b=b.get_bb()
+    
+    if left_a>right_b: return False
+    if right_a<left_b: return False
+    if top_a<bottom_b: return False
+    if bottom_a>top_b: return False
+
+    return True
+    
+    
         
 def update():
-    global moun,jump,jump_time,jump_check,background,flying,sun,moon
+    global running,moun,jump,jump_time,jump_check,background,flying,sun,moon,boy,enemy
     boy.update()
     sun.update()
     moon.update()
     moun.update()
     background.update()
-    flying.update(enemyspeed,check_time)
+    flying.update(enemyspeed)
+    if collide(flying,boy):
+        running=False
+        font.draw(300,200,"Game Over")
+        font.draw(270,150,"Score : ")
+        font.draw(410,150,str(check_time))
+        font.draw(470,150,"Second ")
 
 
 
@@ -130,7 +144,7 @@ def draw():
         font.draw(730,350,str(check_time))
         font.draw(620,350,"Score:")
         boy.draw()
-        #flying.draw()
+        flying.draw()
         update_canvas()
         handle_events()
         delay(0.05)
